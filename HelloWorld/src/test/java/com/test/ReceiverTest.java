0b54f942c3bbe49acc47d83216d0b2c042a0ea97
weimeng <mweidlut@163.com>
2018-03-23 11:16:47 +0800

    add test Consumer

---
+++ HelloWorld/src/test/java/com/test/ReceiverTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import com.google.common.collect.Lists;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+/**
+ * User: weimeng
+ * Date: 2018/3/23 10:46
+ */
+@Ignore
+public class ReceiverTest {
+
+
+    @Test
+    public void testConsumer() {
+
+        ArrayList<String> arrayList = Lists.newArrayList("A", "B");
+
+        arrayList.forEach(((Consumer<String>) s -> {
+            System.out.println(s.toUpperCase());
+        }).andThen(e -> System.out.println(e + "_")));
+
+    }
+}
